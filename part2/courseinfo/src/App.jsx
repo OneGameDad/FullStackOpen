114d700bfbd8e168efcff975febfa8f1a21b4fbd
Gregory Pellechi <greg.pellechi@gmail.com>
2025-11-09 16:39:57 +0200

    Completed exercise 2.1

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,36 +1,4 @@
-const Header = (props) => {
-  return (
-    <h1>{props.course.name}</h1>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>
-      {props.course.name} {props.course.exercises}
-    </p>
-  )
-}
-
-const Content = (props) => {
-  return (
-    <div>
-      <Part course={props.course.parts[0]} />
-      <Part course={props.course.parts[1]} />
-      <Part course={props.course.parts[2]} />
-    </div>
-  )
-}
-
-const Total = (props) => {
-  return (
-    <p>
-      Number of exercises {props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises}
-    </p>
-  )
-}
-
-
+import Course from './components/Course'
 
 const App = () => {
   const course = {
@@ -39,9 +7,11 @@ const App = () => {
     parts: [
       { name: 'Fundamentals of React', exercises: 10, id: 1 },
       { name: 'Using props to pass data', exercises: 7, id: 2 },
-      { name: 'State of a component', exercises: 14, id: 3 }
+      { name: 'State of a component', exercises: 14, id: 3 },
+      { name: 'Defining terms - teamwork & professionalism', exercises: 22, id: 4 }
     ]
   }
+  console.log(course)
   return <Course course={course} />
 }
 
