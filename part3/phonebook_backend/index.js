e9d326009fce188454a7f967755f7b14071b1f51
Gregory Pellechi <greg.pellechi@gmail.com>
2025-11-14 08:39:56 +0200

    Completed 3.3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -34,6 +34,18 @@ app.get('/api/persons', (request, response) => {
 	response.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+	const id = request.params.id
+	const person = persons.find(person => person.id === id)
+
+	if (person) {
+		response.json(person)
+	} else {
+		response.statusMessage = "Could not find that person in the phonebook."
+		response.status(404).end()
+	}
+})
+
 const generateDate = () => {
 	const date = new Date()
 	return date
